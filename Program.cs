be33661953ccee2ce1cc30074bab4473f292392f
kitsune <57214464+Terr0rize@users.noreply.github.com>
2020-11-24 15:37:20 +0400

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+namespace Linst_Lern
+{  
+    class Program
+    {        
+        static void Main()
+        {
+            Cafedra cafedra = new Cafedra();
+            cafedra.Menu();
+        }
+    }
+}
